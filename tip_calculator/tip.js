4efc489369955622382c400615756c6e4c53ddf7
zayyar-miu <zayyar.tun@miu.edu>
2023-03-22 11:14:11 -0500

    fix calculation logic

--- tip_calculator/tip.js
+++ tip_calculator/tip.js
@@ -1,3 +1,3 @@
 function calcTip() {
-    document.getElementById("total").innerHTML = "$" + Math.round((document.getElementById("tip").value / document.getElementById("subtotal").value) * 100).toFixed(2);
+    document.getElementById("total").innerHTML = "$" + Math.round(parseFloat(document.getElementById("subtotal").value) + ((parseFloat(document.getElementById("tip").value) / parseFloat(document.getElementById("subtotal").value)) * 100)).toFixed(2);
 }
